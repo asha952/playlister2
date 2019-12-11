49282a02a0c62e649ea8db043e7dc3bacb67ef00
asha952 <ashabsiddiqui952@gmail.com>
2019-12-10 18:53:13 -0800

    upload 4

--- playlister/app.py
+++ playlister/app.py
@@ -14,6 +14,8 @@ def video_url_creator(id_lst):
         video = 'https://youtube.com/embed/' + vid_id
         videos.append(video)
     return videos
+
+
 '''
 @app.route('/')
 def index():
@@ -28,6 +30,8 @@ playlists = [
     {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
 ]
 '''
+
+
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
@@ -51,6 +55,7 @@ def playlists_submit():
     playlists.insert_one(playlist)
     return redirect(url_for('playlists_index'))
 
+
 # Note the methods parameter that explicitly tells the route that this is a POST
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -67,6 +72,20 @@ def playlists_submit():
     }
     playlists.insert_one(playlist)
     return redirect(url_for('playlists_index'))
+
+
+# Add this with the rest of your import statements
+from bson.objectid import ObjectId
+
+
+# ...
+
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 
 if __name__ == '__main__':
